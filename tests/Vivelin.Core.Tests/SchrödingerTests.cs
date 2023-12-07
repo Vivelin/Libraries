68797f2979286e94ad564d1632a65b3051b3be5a
Laura Verdoes <1766841+Vivelin@users.noreply.github.com>
2023-12-07 17:58:46 +0100

    Add test for manual Resolve call

--- tests/Vivelin.Core.Tests/SchrödingerTests.cs
+++ tests/Vivelin.Core.Tests/SchrödingerTests.cs
@@ -3,6 +3,17 @@ namespace Vivelin.Core.Tests;
 public class SchrödingerTests
 {
     [Fact]
+    public void SchrodingersString_ResolvesToRandomString()
+    {
+        var values = new[] { "Bulbasaur", "Charmander", "Squirtle" };
+        var instance = new Schrödinger<string>(values);
+        var mockRandom = new Mock<Random>();
+        mockRandom.Setup(x => x.Next(3)).Returns(0);
+
+        instance.Resolve(mockRandom.Object).Should().Be("Bulbasaur");
+    }
+
+    [Fact]
     public void SchrodingersString_ImplicitlyResolvesToRandomString()
     {
         var values = new[] { "Bulbasaur", "Charmander", "Squirtle" };
